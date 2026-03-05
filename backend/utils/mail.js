424875a0a4760f557bc83f07f75519eda236692c
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-03-05 22:22:12 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,21 +2,23 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "gmail",
-  port: 465,
-  secure: true, 
-  auth: {
-    user: process.env.Email,
-    pass: process.env.Password,
-  },
-});
 
-export const sendOtpMail = async (to, otp) => {
-    await transporter.sendMail({
-        from:process.env.Email,
-        to,
-        subject:"Reset Your Password",
-        html:`<p>Your otp for password reset is <b>${otp}</b>. it expires in 5 minutes. </p>`
-    });
-}
+export const sendOtpMail = async (email, otp) => {
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Password Reset OTP",
+    text: `Your OTP is ${otp}`
+  };
+
+  await transporter.sendMail(mailOptions);
+};
